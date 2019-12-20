059a8307952ac12247a1fe3ffa5ac0c03060a262
yamakai <restartsugar@gmail.com>
2019-12-20 17:04:56 +0900

    cliにaccess token機能を実装

--- get_code_metrics/cli.py
+++ get_code_metrics/cli.py
@@ -2,16 +2,31 @@ import os.path
 import sys
 from argparse import ArgumentParser
 import re
+from pathlib import Path
 
 
 def command_parser():
     parser = ArgumentParser()
     parser.add_argument('path_to_input_file',
-                        type=str)
+                        type=Path)
+
     parser.add_argument('-o',
                         '--out',
+                        required=True,
+                        type=Path,
+                        help='path to output file')
+
+    parser.add_argument('--token',
+                        '-t',
                         type=str,
-                        default='./result.json')
+                        help='Access Token for GitHub API')
+
+    parser.add_argument('--clone',
+                        '-c',
+                        type=Path,
+                        default='~/.cache/gcm/repositories',
+                        help='specify a directory path for cloning repository.')
+
     parser.add_argument('--force',
                         '-f',
                         action='store_true',
@@ -20,9 +35,20 @@ def command_parser():
     args = parser.parse_args()
 
     repository_list = get_repository_list(args.path_to_input_file)
-    check_directory_exist(args.out, args.force)
 
-    return repository_list, args.out
+    # args.outは最後がfile名なのでdirectoryとfile名を分けてdirectoryを渡す
+    out_path = args.out  # type: Path
+    out_path = out_path.expanduser().resolve()
+    check_directory_exist(out_path.parent)
+
+    # args.cloneはディレクトリなのでそのまま渡す
+    clone_path = args.clone  # type: Path
+    clone_path = clone_path.expanduser().resolve()
+    check_directory_exist(clone_path)
+
+    access_token = get_access_token(args.token)
+
+    return repository_list, out_path, clone_path, access_token
 
 
 def get_repository_list(path_to_input_file: str):
@@ -40,18 +66,28 @@ def get_repository_list(path_to_input_file: str):
     return repository_list
 
 
-def check_directory_exist(path_to_output_file: str, create_force: bool):
-    directory_path = os.path.split(path_to_output_file)[0]
-
-    # コマンドの指定でdirectory pathが存在し，directory pathが実行時の環境に存在せず
-    # かつforceフラグが立っている場合
-    if directory_path and not os.path.exists(directory_path) and create_force:
-        print('make directory!: ', directory_path)
-        os.makedirs(directory_path, exist_ok=True)
+def check_directory_exist(path_to_output_dir: Path):
+    if path_to_output_dir.exists():
         return
 
-    # コマンドの指定でdirectory pathが存在するがdirectory pathが実行時の環境に存在せず
-    # かつforceフラグが立っていない場合
-    if not os.path.exists(directory_path) and not create_force:
-        print('Error: specified directory does not exit', file=sys.stderr)
+    path_to_output_dir.mkdir(parents=True)
+    print('make directory: ', path_to_output_dir)
+
+
+def check_filename_exist(filename: str):
+
+    if filename == '' or filename == '.':
+        print('Error: You don\'t specify filename', file=sys.stderr)
         sys.exit(1)
+    else:
+        return
+
+
+def get_access_token(token: str):
+    env_token = os.getenv('GCM_GITHUB_TOKEN')
+    if token is None or len(token) == 0:
+        if env_token is None or len(env_token) == 0:
+            print('Error: No GitHub Access Token is specified.', file=sys.stderr)
+            exit(1)
+        return env_token
+    return token
