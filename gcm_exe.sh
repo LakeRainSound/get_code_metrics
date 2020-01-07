4ea730c37c60eda9e638fa117e80010478cd61b6
yamakai <restartsugar@gmail.com>
2020-01-07 16:54:45 +0900

    shellスクリプトの変更．

--- gcm_exe.sh
+++ gcm_exe.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-pipenv run gcm contributor600.txt --out contributor.json
-pipenv run gcm fork600.txt --out fork.json
-pipenv run gcm dependent600.txt --out dependent.json
-pipenv run gcm watcher600.txt --out watcher.json
-pipenv run gcm star600.txt --out star.json
+gcm dataset/contributor600.txt --out contributor.json
+gcm dataset/fork600.txt --out fork.json
+gcm dataset/dependent600.txt --out dependent.json
+gcm dataset/watcher600.txt --out watcher.json
+gcm dataset/star600.txt --out star.json
